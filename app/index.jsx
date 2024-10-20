0892aae22a5b4301e43588b75f662b1ad3aeb88f
Uriah Rodriguez <urro200@gmail.com>
2024-10-19 21:58:14 -0700

    Creating Tab Nav bar Bottom Screen
    Creating the Tab Nav Bar, Home, Book, Create, Profile.

--- app/index.jsx
+++ app/index.jsx
@@ -7,8 +7,8 @@ export default function App() {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-pblack">Welcome to HoopNow</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: "blue" }}>
-        Go to profile
+      <Link href="/home" style={{ color: "blue" }}>
+        Navigate Home
       </Link>
     </View>
   );
